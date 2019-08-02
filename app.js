b73aa70ce995be2d8c4be38a7dbe247e94e6ed65
gasgas1 <gaspare.mule1@gmail.com>
2019-08-02 11:39:25 +0200

    Rimosso router/user.js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-var express = require('express');
+const express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -15,6 +14,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
